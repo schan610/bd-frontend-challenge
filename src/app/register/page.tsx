1cc8d391e3a810e2bca7c7f2c8c608953986f717
Sabrina Chan <sabrinachan.cs@gmail.com>
2024-05-10 11:59:44 -0700

    feat: add control form section

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { Fragment, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const steps = [
   {id: "1", title: "Account"},
@@ -9,7 +11,15 @@ const steps = [
 
 const RegisterPage = () => {
   const [curStep, setCurStep] = useState(0)
+  const handleNextBtn = () => {
+    if(curStep === steps.length-1) return;
+    setCurStep(prevState => prevState+1);
+  };
+  const handleBackBtn = () => {
+    if(curStep === 0) return;
 
+    setCurStep(prevState => prevState-1);
+  };
   return <div className="w-screen h-screen flex flex-col justify-center items-center max-w-2xl mx-auto">
     <section className="section-container">
       <div className="step-header">
@@ -24,6 +34,10 @@ const RegisterPage = () => {
             </Fragment>
           } )}
         </div>
+      </div>
+      <div className="controls">
+          <button className="controls__btn--secondary" onClick={handleBackBtn}><FontAwesomeIcon icon={faArrowLeft} /> Back</button>
+          <button className="controls__btn--primary" onClick={handleNextBtn}>Next Step <FontAwesomeIcon icon={faArrowRight} /></button>
       </div>
 
   </section>
